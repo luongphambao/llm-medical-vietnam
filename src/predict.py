e53eb44c10fa7b821cd52db1a769a766c82d64e9
Luong Pham Bao <58712033+luongphambao@users.noreply.github.com>
2024-06-30 22:40:39 +0700

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np 
 from tqdm import tqdm
 from dotenv import load_dotenv
+import argparse
 try:
     from src.cod import get_summarize
     from src.utils import *
@@ -22,59 +23,50 @@ except:
 
 
 load_dotenv(".env")
-
+def parse_args():
+    parser = argparse.ArgumentParser(description="Embedding")
+    parser.add_argument("--model_embedding_name", type=str, default="BAAI/bge-m3", help="model name")
+    parser.add_argument("--cache_dir", type=str, default="cache/", help="cache directory")
+    parser.add_argument("--persist_directory", type=str, default="chroma_db_bge", help="persist directory")
+    parser.add_argument("--corpus_path", type=str, default="corpus_summarize", help="corpus path")
+    parser.add_argument("--csv_path", type=str, default="data/public_test.csv", help="csv path")
+    parser.add_argument("--output_path", type=str, default="submit.csv", help="output path")
+    parser.add_argument("--model_name", type=str, default="BAAI/bge-m3", help="model name")
+    parser.add_argument("--k1", type=int, default=5, help="k1")
+    parser.add_argument("--k2", type=int, default=5, help="k2")
+    return parser.parse_args()
 def main():
+    args = parse_args()
     google_api_key = os.getenv("GOOGLE_API_KEY")
     openai_api_key = os.getenv("OPENAI_API_KEY")
-    corpus_path = 'corpus_summarize/'
+    corpus_path = args.corpus_path
     docs,texts = load_corpus(corpus_path)
     print("Loaded corpus")
     splits =texts 
-    embedding = Embedding(model_name="BAAI/bge-m3", device='cpu', cache_dir="cache/", persist_directory="chroma_db_bge_v2")
-    #embedding  = Embedding(model_name="google", device='cpu', cache_dir="cache/", persist_directory="chroma_db_google")
-    #embedding =   Embedding(model_name="openai", device='cpu', cache_dir="cache/", persist_directory="chroma_db_openai",openai_api_key=openai_api_key)
-    print(embedding.model_name)
+    embedding = Embedding(model_name=args.model_embedding_name, cache_dir=args.cache_dir, persist_directory=args.persist_directory,openai_api_key=openai_api_key)
     vectordb = embedding.load_embedding()
     print("Loaded embedding")
-    search = Searching(2,2,vectordb,splits)
+    k1 = args.k1
+    k2 = args.k2
+    search = Searching(k1,k2,vectordb,splits)
     print("Loaded search")
-    df = pd.read_csv("data/public_test.csv")
+    df = pd.read_csv(args.csv_path)
     result = {"id": [], "answer": []}
-    model = LLM(google_api_key=google_api_key)
-    #model = LLM(openai_api_key=openai_api_key)
-    #model_name ="ontocord/vistral"
-    model_name ="mrjacktung/phogpt-4b-chat-gguf"
-    model = LLM(model_name=model_name,ollama_use=True)
+    model = LLM(openai_api_key=openai_api_key,google_api_key=google_api_key,model_name =args.model_name)
+
     print("Loaded model")
     for index, row in tqdm(df.iterrows()):
         result["id"].append(row["id"].strip())
-        # try:
-            
         question, choices, num_choices = process_single_row(row)
         docs = search.hybrid_search(question)
         context = search.get_context(docs)
-        #context = get_summarize(context)
-        #print("Summarized context",context)
         question = preprocess_question(question)
         prompt = model.preprocess_prompt(question, choices, context)
-        # with open("prompt_sample.txt", "w") as f:
-        #     f.write(prompt)
         answer = model.generate(prompt)
         output_json = process_output(answer, num_choices)
-        #print(output_json)
         result["answer"].append(output_json)
-        #result_path
-        # #except:
-        #     print("Error at index: ", index)
-        #     ##result["id"].append(row["id"].strip())
-        #     value = "0"*num_choices
-        #     result["answer"].append(value)
-        #     continue
-        #break
     newdf = pd.DataFrame(result, dtype=str)
-    newdf.to_csv("submit_phogpt.csv", index=False)
-# query ="Đàn ông có thể bị mắc ung thư vú không?"
-# vertor_result_docs = search.hybrid_search(query)
-# print(vertor_result_docs[0].page_content)
+    newdf.to_csv(args.output_path, index=False)
 if __name__ == "__main__":
+    #python3 src/predict.py --model_embedding_name="BAAI/bge-m3" --cache_dir="cache/" --persist_directory="chroma_db_bge" --corpus_path="corpus_summarize" --csv_path="data/public_test.csv" --output_path="submit.csv" --corpus_path="corpus_summarize" --model_name="BAAI/bge-m3"
     main()
